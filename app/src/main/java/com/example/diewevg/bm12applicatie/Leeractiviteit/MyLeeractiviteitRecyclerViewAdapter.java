2fc8a722ec49b83716e00da4f11713f706d732b2
diewevg <diewe.vangeffen@outlook.com>
2018-01-18 16:14:05 +0100

    views added and mapstructure updated

---
+++ app/src/main/java/com/example/diewevg/bm12applicatie/Leeractiviteit/MyLeeractiviteitRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package com.example.diewevg.bm12applicatie.Leeractiviteit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.diewevg.bm12applicatie.R;
+import com.example.diewevg.bm12applicatie.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link LeeractiviteitFragment.OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyLeeractiviteitRecyclerViewAdapter extends RecyclerView.Adapter<MyLeeractiviteitRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final LeeractiviteitFragment.OnListFragmentInteractionListener mListener;
+
+    public MyLeeractiviteitRecyclerViewAdapter(List<DummyItem> items, LeeractiviteitFragment.OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_leeractiviteit, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).cursus);
+        holder.mContentView.setText(mValues.get(position).leeractiviteit);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
--- app/src/main/java/com/example/diewevg/bm12applicatie/MainActivity.java
+++ app/src/main/java/com/example/diewevg/bm12applicatie/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.diewevg.bm12applicatie;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,22 +15,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.diewevg.bm12applicatie.Leeractiviteit.LeeractiviteitFragment;
+import com.example.diewevg.bm12applicatie.Resultaten.Resultaat;
+import com.example.diewevg.bm12applicatie.dummy.DummyContent;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    TextView testView;
+    implements
+        Resultaat.OnFragmentInteractionListener,
+        LeeractiviteitFragment.OnListFragmentInteractionListener,
+        //LesRooster.OnFragmentInteractionListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        testView = (TextView) findViewById(R.id.testView);
         setSupportActionBar(toolbar);
 
-        //testView.setText("hoi");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,20 +91,38 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            testView.setText("Leeractiviteit");
-        } else if (id == R.id.nav_gallery) {
-            testView.setText("Resultaat");
-        }  else if (id == R.id.nav_manage) {
-            testView.setText("Lesrooster");
+        Fragment fragment = null;
+        if (id == R.id.leeractiviteit) {
+            fragment = new LeeractiviteitFragment();
+        } else if (id == R.id.resultaat) {
+            fragment = new Resultaat();
+        } else if (id == R.id.lesrooster) {
+
         } else if (id == R.id.nav_share) {
+
 
         } else if (id == R.id.nav_send) {
 
         }
 
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frame_container, fragment).commit();
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
